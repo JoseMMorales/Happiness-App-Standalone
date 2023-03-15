e87cb2652cdf82cbf6068b2fb51b145ec13817e0
JoseMMorales <josemulamorales33@hotmail.com>
2023-03-15 09:57:00 +0100

    Add Spinner to home page

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+
 import { PeopleTableComponent } from 'src/app/components';
+import { SpinnerComponent } from 'src/app/components/spinner';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, PeopleTableComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    PeopleTableComponent,
+    SpinnerComponent,
+  ],
 })
 export class HomeModule {}
